f4333cd1e6c6dc7e92c29db7b73ae7e5329be49f
peerdavid <david@peer.tirol>
2016-07-14 16:13:33 +0200

    Testing now with mnist code

--- train.py
+++ train.py
@@ -7,8 +7,18 @@
 # 2 = estate
 
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import gzip
+import sys
+import tarfile
+from six.moves import urllib
+
 
 import os
+import re
 import traceback
 import tensorflow as tf
 from tensorflow.python.framework import ops
@@ -17,10 +27,15 @@ from tensorflow.python.training import queue_runner
 from tensorflow.python.ops import random_ops
 
 
-NUM_CLASSES = 10
+NUM_CLASSES = 3
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 
+
+# If a model is trained with multiple GPUs, prefix all Op names with tower_name
+# to differentiate the operations. Note that this prefix is removed from the
+# names of the summaries when visualizing a model.
+TOWER_NAME = 'tower'
 
 def read_labeled_image_list(path):
     """Reads images and labels from file system. Create a folder for each label and put 
@@ -57,9 +72,111 @@ def read_images_from_disk(input_queue):
     example = tf.image.decode_png(file_contents, channels=3)
     return example, label 
 
+#
+# Create some wrappers for simplicity
+#
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
 
 
+def maxpool2d(x, k=2):
+    # MaxPool2D wrapper
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                          padding='SAME')
 
+
+# Create model
+def inference(x, weights, biases, dropout):
+    # Reshape input picture
+    #x = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    # Convolution Layer
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1, k=2)
+
+    # Convolution Layer
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2, k=2)
+
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    # Apply Dropout
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    # Output, class prediction
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
+    
+    # https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/convolutional_network.py
+ # Store layers weight & bias
+weights = {
+    # 5x5 conv, 1 input, 32 outputs
+    'wc1': tf.Variable(tf.random_normal([5, 5, 3, 32])),
+    # 5x5 conv, 32 inputs, 64 outputs
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    # fully connected, 7*7*64 inputs, 1024 outputs
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    # 1024 inputs, 10 outputs (class prediction)
+    'out': tf.Variable(tf.random_normal([1024, NUM_CLASSES]))
+}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([NUM_CLASSES]))
+}
+   
+   
+   
+def loss(logits, labels):
+    """Calculates the loss from the logits and the labels.
+    Args:
+    logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+    labels: Labels tensor, int32 - [batch_size].
+    Returns:
+    loss: Loss tensor of type float.
+    """
+    labels = tf.to_int64(labels)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits, labels, name='xentropy')
+    loss = tf.reduce_mean(cross_entropy, name='xentropy_mean')
+    return loss
+  
+
+def training(loss, learning_rate):
+    """Sets up the training Ops.
+    Creates a summarizer to track the loss over time in TensorBoard.
+    Creates an optimizer and applies the gradients to all trainable variables.
+    The Op returned by this function is what must be passed to the
+    `sess.run()` call to cause the model to train.
+    Args:
+    loss: Loss tensor, from loss().
+    learning_rate: The learning rate to use for gradient descent.
+    Returns:
+    train_op: The Op for training.
+    """
+    # Add a scalar summary for the snapshot loss.
+    tf.scalar_summary(loss.op.name, loss)
+    # Create the gradient descent optimizer with the given learning rate.
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    # Create a variable to track the global step.
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    # Use the optimizer to apply the gradients that minimize the loss
+    # (and also increment the global step counter) as a single training step.
+    train_op = optimizer.minimize(loss, global_step=global_step)
+    return train_op
+  
+  
+# https://github.com/tensorflow/tensorflow/blob/r0.9/tensorflow/examples/tutorials/mnist/fully_connected_feed.py
 #
 # M A I N
 #
@@ -68,18 +185,18 @@ if __name__ == '__main__':
         # Reads pfathes of images together with their labels
         image_list, label_list = read_labeled_image_list("data/")
 
-        images = ops.convert_to_tensor(image_list, dtype=dtypes.string)
-        labels = ops.convert_to_tensor(label_list, dtype=dtypes.int32)
+        tf_images = ops.convert_to_tensor(image_list, dtype=dtypes.string)
+        tf_labels = ops.convert_to_tensor(label_list, dtype=dtypes.int32)
 
         # Makes an input queue
-        input_queue = tf.train.slice_input_producer([images, labels],
+        input_queue = tf.train.slice_input_producer([tf_images, tf_labels],
                                                     shuffle=True)
 
         image, label = read_images_from_disk(input_queue)
 
         # Randomly crop a [height, width] section of the image.
-        height = 100 
-        width = 120
+        height = 28 
+        width = 28
         distorted_image = tf.random_crop(image, [height, width, 3])
         
         # Image processing for evaluation.
@@ -106,13 +223,24 @@ if __name__ == '__main__':
         # Display the training images in the visualizer.
         tf.image_summary('images', image_batch)
         
-        ret1 = image_batch
-        ret2 = tf.reshape(label_batch, [batch_size])
-         
-         
-        #with tf.Session() as sess:
-        #    result = sess.run(input_queue)
-            
+        images = image_batch
+        labels = tf.reshape(label_batch, [batch_size])
+        
+        
+        #
+        # Construct model
+        #
+        # Parameters
+        learning_rate = 0.001
+        training_iters = 200000
+        display_step = 10
+
+        keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
+        logits = inference(images, weights, biases, keep_prob)
+        loss = loss(logits, labels)
+        training = training(loss, learning_rate)
+
+
     except:
         traceback.print_exc()
 
