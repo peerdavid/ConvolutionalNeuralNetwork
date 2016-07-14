1689a004f4ff0b6265a397a8393a6e0efe17fd1a
peerdavid <david@peer.tirol>
2016-07-14 18:47:57 +0200

    Loss decreasing ;)

--- train.py
+++ train.py
@@ -16,6 +16,8 @@ import os
 import math
 import traceback
 import time
+from datetime import datetime
+import re
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 from tensorflow.python.framework import ops
@@ -38,9 +40,7 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
 flags.DEFINE_integer('max_steps', 2000, 'Number of steps to run trainer.')
-flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')
-flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
-flags.DEFINE_integer('batch_size', 100, 'Batch size.  '
+flags.DEFINE_integer('batch_size', 128, 'Batch size.  '
                      'Must divide evenly into the dataset sizes.')
 flags.DEFINE_string('train_dir', 'data', 'Directory to put the training data.')
 flags.DEFINE_boolean('fake_data', False, 'If true, uses fake data '
@@ -85,47 +85,155 @@ def read_images_from_disk(input_queue):
     """
     label = input_queue[1]
     file_contents = tf.read_file(input_queue[0])
-    example = tf.image.decode_png(file_contents, channels=3)
-    return example, label 
+    decoded = tf.image.decode_jpeg(file_contents, channels=3)
+    decoded.set_shape([IMAGE_SIZE, IMAGE_SIZE, 3])
+    return decoded, label 
 
 
-def inference(images, hidden1_units, hidden2_units):
-    """Build the MNIST model up to where it may be used for inference.
+def _activation_summary(x):
+    """Helper to create summaries for activations.
+
+    Creates a summary that provides a histogram of activations.
+    Creates a summary that measure the sparsity of activations.
+
     Args:
-        images: Images placeholder, from inputs().
-        hidden1_units: Size of the first hidden layer.
-        hidden2_units: Size of the second hidden layer.
+        x: Tensor
     Returns:
-        softmax_linear: Output tensor with the computed logits.
+        nothing
     """
-    # Hidden 1
-    with tf.name_scope('hidden1'):
-        weights = tf.Variable(
-            tf.truncated_normal([IMAGE_PIXELS, hidden1_units],
-                                stddev=1.0 / math.sqrt(float(IMAGE_PIXELS))),
-            name='weights')
-        biases = tf.Variable(tf.zeros([hidden1_units]),
-                            name='biases')
-        hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
-    # Hidden 2
-    with tf.name_scope('hidden2'):
-        weights = tf.Variable(
-            tf.truncated_normal([hidden1_units, hidden2_units],
-                                stddev=1.0 / math.sqrt(float(hidden1_units))),
-            name='weights')
-        biases = tf.Variable(tf.zeros([hidden2_units]),
-                            name='biases')
-        hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
-    # Linear
-    with tf.name_scope('softmax_linear'):
-        weights = tf.Variable(
-            tf.truncated_normal([hidden2_units, NUM_CLASSES],
-                                stddev=1.0 / math.sqrt(float(hidden2_units))),
-            name='weights')
-        biases = tf.Variable(tf.zeros([NUM_CLASSES]),
-                            name='biases')
-        logits = tf.matmul(hidden2, weights) + biases
-    return logits
+    # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
+    # session. This helps the clarity of presentation on tensorboard.
+    tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+    tf.histogram_summary(tensor_name + '/activations', x)
+    tf.scalar_summary(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+
+
+def _variable_on_cpu(name, shape, initializer):
+    """Helper to create a Variable stored on CPU memory.
+
+    Args:
+        name: name of the variable
+        shape: list of ints
+        initializer: initializer for Variable
+
+    Returns:
+        Variable Tensor
+    """
+    with tf.device('/cpu:0'):
+        dtype = tf.float32 #tf.float16 if FLAGS.use_fp16 else tf.float32
+        var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+    return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+    """Helper to create an initialized Variable with weight decay.
+
+    Note that the Variable is initialized with a truncated normal distribution.
+    A weight decay is added only if one is specified.
+
+    Args:
+        name: name of the variable
+        shape: list of ints
+        stddev: standard deviation of a truncated Gaussian
+        wd: add L2Loss weight decay multiplied by this float. If None, weight
+            decay is not added for this Variable.
+
+    Returns:
+        Variable Tensor
+    """
+    dtype = tf.float32 #tf.float16 if FLAGS.use_fp16 else tf.float32
+    var = _variable_on_cpu(
+        name,
+        shape,
+        tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
+    if wd is not None:
+        weight_decay = tf.mul(tf.nn.l2_loss(var), wd, name='weight_loss')
+        tf.add_to_collection('losses', weight_decay)
+    return var
+  
+  
+
+def inference(images):
+    """Build the CIFAR-10 model.
+
+    Args:
+        images: Images returned from distorted_inputs() or inputs().
+
+    Returns:
+        Logits.
+    """
+    # We instantiate all variables using tf.get_variable() instead of
+    # tf.Variable() in order to share variables across multiple GPU training runs.
+    # If we only ran this model on a single GPU, we could simplify this function
+    # by replacing all instances of tf.get_variable() with tf.Variable().
+    #
+    # conv1
+    with tf.variable_scope('conv1') as scope:
+        kernel = _variable_with_weight_decay('weights',
+                                            shape=[5, 5, 3, 64],
+                                            stddev=5e-2,
+                                            wd=0.0)
+        conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
+        bias = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv1)
+
+    # pool1
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                            padding='SAME', name='pool1')
+    # norm1
+    norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                        name='norm1')
+
+    # conv2
+    with tf.variable_scope('conv2') as scope:
+        kernel = _variable_with_weight_decay('weights',
+                                            shape=[5, 5, 64, 64],
+                                            stddev=5e-2,
+                                            wd=0.0)
+        conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv2)
+
+    # norm2
+    norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                        name='norm2')
+    # pool2
+    pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
+                            strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+
+    # local3
+    with tf.variable_scope('local3') as scope:
+        # Move everything into depth so we can perform a single matrix multiply.
+        reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
+        dim = reshape.get_shape()[1].value
+        weights = _variable_with_weight_decay('weights', shape=[dim, 384],
+                                            stddev=0.04, wd=0.004)
+        biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+        local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+        _activation_summary(local3)
+
+    # local4
+    with tf.variable_scope('local4') as scope:
+        weights = _variable_with_weight_decay('weights', shape=[384, 192],
+                                            stddev=0.04, wd=0.004)
+        biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+        local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+        _activation_summary(local4)
+
+    # softmax, i.e. softmax(WX + b)
+    with tf.variable_scope('softmax_linear') as scope:
+        weights = _variable_with_weight_decay('weights', [192, NUM_CLASSES],
+                                            stddev=1/192.0, wd=0.0)
+        biases = _variable_on_cpu('biases', [NUM_CLASSES],
+                                tf.constant_initializer(0.0))
+        softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
+        _activation_summary(softmax_linear)
+
+    return softmax_linear
    
    
    
@@ -236,68 +344,68 @@ def fill_feed_dict(data_set, images_pl, labels_pl):
 #
 if __name__ == '__main__':
     try:
-        # Reads pfathes of images together with their labels
-        image_list, label_list = read_labeled_image_list("data/")
-
-        tf_images = ops.convert_to_tensor(image_list, dtype=dtypes.string)
-        tf_labels = ops.convert_to_tensor(label_list, dtype=dtypes.int32)
-
-        # Makes an input queue
-        input_queue = tf.train.slice_input_producer([tf_images, tf_labels],
-                                                    shuffle=True)
-
-        image, label = read_images_from_disk(input_queue)
-
-        # Randomly crop a [height, width] section of the image.
-        distorted_image = tf.random_crop(image, [IMAGE_SIZE, IMAGE_SIZE, 3])
-        
-        # Image processing for evaluation.
-        # Crop the central [height, width] of the image.
-        resized_image = tf.image.resize_image_with_crop_or_pad(distorted_image, IMAGE_SIZE, IMAGE_SIZE)
-
-        # Subtract off the mean and divide by the variance of the pixels.
-        float_image = tf.image.per_image_whitening(resized_image)
-
-        # Ensure that the random shuffling has good mixing properties.
-        num_preprocess_threads = 16
-        min_fraction_of_examples_in_queue = 0.4
-        min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *
-                                min_fraction_of_examples_in_queue)
-                                
-        image_batch, label_batch = tf.train.shuffle_batch(
-            [float_image, label], 
-            num_threads = num_preprocess_threads,
-            capacity=min_queue_examples + 3 * FLAGS.batch_size,
-            min_after_dequeue=min_queue_examples,
-            batch_size=FLAGS.batch_size)
-                                               
-        # Display the training images in the visualizer.
-        tf.image_summary('images', image_batch)
-        
-        images = image_batch
-        labels = tf.reshape(label_batch, [FLAGS.batch_size])
-        
-        
         # https://github.com/tensorflow/tensorflow/blob/r0.9/tensorflow/examples/tutorials/mnist/fully_connected_feed.py
         # Tell TensorFlow that the model will be built into the default Graph.
         with tf.Graph().as_default():
+            # Reads pfathes of images together with their labels
+            image_list, label_list = read_labeled_image_list("data/")
+
+            tf_images = ops.convert_to_tensor(image_list, dtype=dtypes.string)
+            tf_labels = ops.convert_to_tensor(label_list, dtype=dtypes.int32)
+
+            # Makes an input queue
+            input_queue = tf.train.slice_input_producer([tf_images, tf_labels],
+                                                        shuffle=True)
+
+            image, label = read_images_from_disk(input_queue)
+
+            reshaped_image = tf.cast(image, tf.float32)
+
+            # Randomly crop a [height, width] section of the image.
+            #distorted_image = tf.random_crop(reshaped_image, [IMAGE_SIZE, IMAGE_SIZE, 3])
+            
+            # Image processing for evaluation.
+            # Crop the central [height, width] of the image.
+            resized_image = tf.image.resize_image_with_crop_or_pad(reshaped_image, IMAGE_SIZE, IMAGE_SIZE)
+
+            # Subtract off the mean and divide by the variance of the pixels.
+            float_image = tf.image.per_image_whitening(resized_image)
+
+            # Ensure that the random shuffling has good mixing properties.
+            num_preprocess_threads = 16
+            min_fraction_of_examples_in_queue = 0.4
+            min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *
+                                    min_fraction_of_examples_in_queue)
+                                    
+            image_batch, label_batch = tf.train.shuffle_batch(
+                [float_image, label], 
+                num_threads = num_preprocess_threads,
+                capacity=min_queue_examples + 3 * FLAGS.batch_size,
+                min_after_dequeue=min_queue_examples,
+                batch_size=FLAGS.batch_size)
+                                                
+            # Display the training images in the visualizer.
+            tf.image_summary('images', image_batch)
+            
+            images = image_batch
+            labels = tf.reshape(label_batch, [FLAGS.batch_size])
+        
+       
             # Generate placeholders for the images and labels.
-            images_placeholder, labels_placeholder = placeholder_inputs(
-                FLAGS.batch_size)
+            #images_placeholder, labels_placeholder = placeholder_inputs(
+            #    FLAGS.batch_size)
 
             # Build a Graph that computes predictions from the inference model.
-            logits = inference(images_placeholder,
-                                    FLAGS.hidden1,
-                                    FLAGS.hidden2)
+            logits = inference(images)
 
             # Add to the Graph the Ops for loss calculation.
-            loss = loss(logits, labels_placeholder)
+            loss = loss(logits, labels)
 
             # Add to the Graph the Ops that calculate and apply gradients.
             train_op = training(loss, FLAGS.learning_rate)
 
             # Add the Op to compare the logits to the labels during evaluation.
-            eval_correct = evaluation(logits, labels_placeholder)
+            #eval_correct = evaluation(logits, labels_placeholder)
 
             # Build the summary operation based on the TF collection of Summaries.
             summary_op = tf.merge_all_summaries()
@@ -319,25 +427,24 @@ if __name__ == '__main__':
             # Run the Op to initialize the variables.
             sess.run(init)
 
+            # Start the queue runners.
+            tf.train.start_queue_runners(sess=sess)
+    
             # Start the training loop.
             for step in xrange(FLAGS.max_steps):
                 start_time = time.time()
-      
-                # Fill a feed dictionary with the actual set of images and labels
-                # for this particular training step.
-                feed_dict = fill_feed_dict(data_sets.train,
-                                            images_placeholder,
-                                            labels_placeholder)
-
-                # Run one step of the model.  The return values are the activations
-                # from the `train_op` (which is discarded) and the `loss` Op.  To
-                # inspect the values of your Ops or variables, you may include them
-                # in the list passed to sess.run() and the value tensors will be
-                # returned in the tuple from the call.
-                _, loss_value = sess.run([train_op, loss],
-                                        feed_dict=feed_dict)
-
+                _, loss_value = sess.run([train_op, loss])
                 duration = time.time() - start_time
+                
+                if step % 10 == 0:
+                    num_examples_per_step = FLAGS.batch_size
+                    examples_per_sec = num_examples_per_step / duration
+                    sec_per_batch = float(duration)
+
+                    format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                                'sec/batch)')
+                    print (format_str % (datetime.now(), step, loss_value,
+                                        examples_per_sec, sec_per_batch))
 
     except:
         traceback.print_exc()
